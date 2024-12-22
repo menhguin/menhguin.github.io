564f156bb7195300c5c1ca14a0c6f901a1503f45
menhguin <minh1228@gmail.com>
2024-12-23 01:32:40 +0800

    fifth attempt - using instruction docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+<<<<<<< HEAD
+  site: 'https://menhguin.github.io'
+  // Note: No base parameter needed since this is the main github.io site
+=======
+>>>>>>> 631b229d17949831d47d5f72d6d9edd357019b5b
 });
